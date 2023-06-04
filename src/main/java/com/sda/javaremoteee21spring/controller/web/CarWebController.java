f06544917fd6bd90932685b03e0468d5d264b2de
AlmarVasar <almar.vasar@gmail.com>
2023-06-04 12:58:37 +0300

    Using Thymeleaf to generate dynamic pages

---
+++ src/main/java/com/sda/javaremoteee21spring/controller/web/CarWebController.java
@@ -0,0 +1,25 @@
+package com.sda.javaremoteee21spring.controller.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@Slf4j
+@RequestMapping("/web")
+public class CarWebController {
+
+    @GetMapping({"/home-page", ""})
+    public String homePage(Model data,
+                           @RequestParam(value = "name", defaultValue = "Almar") String name,
+                           @RequestParam(value = "surname", defaultValue = "V.") String surname) {
+        log.info("home page");
+
+        data.addAttribute("name", name);
+        data.addAttribute("surname", surname);
+        return "home-page";
+    }
+}
